96a7aebfad689e94228c24a727723d05af46e3df
Erick Oliveira <erick.loliveira@hotmail.com>
2024-05-22 22:53:36 -0300

    feat: removed docker and added a list for the admin, still need to add one function to edit the users that the list brings

--- src/web/src/app/components/admin-user-list/admin-user-list.component.ts
+++ src/web/src/app/components/admin-user-list/admin-user-list.component.ts
@@ -2,20 +2,26 @@ import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AdminService} from "../../services/AdminService";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatDivider} from "@angular/material/divider";
+import {MatIcon} from "@angular/material/icon";
+import {MatFabButton, MatMiniFabButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-admin-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle, MatDivider, MatIcon, MatFabButton, MatMiniFabButton],
   templateUrl: './admin-user-list.component.html',
   styleUrl: './admin-user-list.component.scss',
 })
 export class AdminUserListComponent implements OnInit{
+  users: any[] = [];
   constructor(private adminService: AdminService) {
   }
   ngOnInit(){
   this.adminService.getAllUsers().subscribe({
     next: data => {
+      this.users = data;
       console.log(data);
     },
     error: error =>{
